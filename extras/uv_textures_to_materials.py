ba15b9cd9b51687e9a3968d2763e128306a22c24
Richard Qian <richwiki101@gmail.com>
2020-08-24 00:11:42 -0500

    uv_textures_to_materials: Refactor, and add the following new features:
    * Support more than one image per mesh
    * Custom STK properties are copied from each image to newly-created material
    * Ignore library/linked meshes and materials
    * Ignore any meshes without faces and/or UV textures
    * Can skip over faces that don't have UV textures assigned

--- extras/uv_textures_to_materials.py
+++ extras/uv_textures_to_materials.py
@@ -1,9 +1,14 @@
 # To be run for STK Blender source files before being opened in 2.80+
 # Searches for UV textures, creates materials out of them, and assigns
 # those materials to all meshes using them
+#
+# Limitations (most of which can be fixed after migration):
+# * Vertex colors are not supported, but they are left intact
+# * Decal shaders (two UV maps) are not supported
+
 import bpy
 
-def createNodeBasedMaterial(material, im):
+def createImageMaterial(material, image):
     material.use_nodes = True
     nodes = material.node_tree.nodes
     links = material.node_tree.links
@@ -30,42 +35,58 @@ def createNodeBasedMaterial(material, im):
     links.new(principled_node.outputs[0], node_output.inputs[0])
 
     tex1_node = nodes.new(type="ShaderNodeTexImage")
-    tex1_node.image = im
+    tex1_node.image = image
     links.new(tex1_node.outputs["Color"], principled_node.inputs["Base Color"])
 
     uvmap_node = nodes.new(type="ShaderNodeUVMap")
     uvmap_node.uv_map = "UVMap" # first UV map for first texture
     links.new(uvmap_node.outputs[0], tex1_node.inputs["Vector"])
 
-# Remove blank materials
-for mat in bpy.data.materials:
-    if "Material" in mat.name:
-        bpy.data.materials.remove(mat)
+# Step 1: Search through all images and create materials out of them
+# Create a node-based material per image, reusing materials if already created
+for image in bpy.data.images:
+    im_name = image.name.split('.')[0]
+    if im_name in bpy.data.materials:
+        material = bpy.data.materials[im_name]
+        # Overwrite the old material if it is not using nodes
+        if not material.use_nodes and material.library is None:
+            createImageMaterial(material, image)
+    else:
+        material = bpy.data.materials.new(im_name)
+        createImageMaterial(material, image)
 
-for obj in bpy.data.objects:
-    if (obj.type == 'MESH'):
+    # Copy the custom properties from image to material if they exist
+    if len(image.keys()) > 0:
+        for prop in image.keys():
+            material[prop] = image[prop]
+
+# Step 2: Run though all non-library meshes with faces and assign the created materials
+# Only meshes with UV maps will be dealt with
+# Each material is to be assigned by image, in case a mesh uses at least two images
+for mesh in bpy.data.meshes:
+    if mesh.library is None and len(mesh.polygons) > 0 and len(mesh.uv_textures) > 0:
+        # Clear all existing materials for a fresh start
+        mesh.materials.clear()
+
         # Search for all images used per mesh
         images = set()
-        for uv_tex in obj.data.uv_textures.active.data:
-            if uv_tex.image.name not in images:
-                images.add(uv_tex.image)
+        for uv_tex in mesh.uv_textures[0].data:
+            try:
+                if uv_tex.image.name not in images:
+                    images.add(uv_tex.image)
+            except:
+                # Face does not have an image assigned
+                continue
 
-        print(obj.data.name)
-        print(images)
-
-        # Create a node-based material per image, reusing materials if already created
         for im in images:
             im_name = im.name.split('.')[0]
-            if im_name in bpy.data.materials:
-                material = bpy.data.materials[im_name]
-                # Overwrite the old material if it is not using nodes
-                if not material.use_nodes:
-                    createNodeBasedMaterial(material, im)
-            else:
-                material = bpy.data.materials.new(im_name)
-                createNodeBasedMaterial(material, im)
+            material = bpy.data.materials[im_name]
 
-            if im_name not in obj.data.materials and len(obj.data.materials) < 1:
-                obj.data.materials.append(material)
-            elif len(obj.data.materials) > 0:
-                obj.material_slots[0].material = material
+            # Add the material to the current mesh if not already there
+            if im_name not in mesh.materials:
+                mesh.materials.append(material)
+
+            # Assign current material using the current image
+            for polygon in mesh.polygons:
+                if mesh.uv_textures.active.data[polygon.index].image == im:
+                    polygon.material_index = mesh.materials.find(im_name)
